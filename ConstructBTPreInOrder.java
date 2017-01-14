c0dfb5be42566d0164d80cbadc77426671d0a43d
lylx1981 <drxuli@gmail.com>
2017-01-14 09:38:31 -0500

    Update ConstructBTPreInOrder.java

--- ConstructBTPreInOrder.java
+++ ConstructBTPreInOrder.java
@@ -8,23 +8,37 @@ Now we know that IN[5] is root, so we know that IN[0] - IN[4] is on the left sid
 Recursively doing this on subarrays, we can build a tree out of it :)
 */
 
-
-public TreeNode buildTree(int[] preorder, int[] inorder) {
-    return helper(0, 0, inorder.length - 1, preorder, inorder);
-}
-
-public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-    if (preStart > preorder.length - 1 || inStart > inEnd) {
-        return null;
-    }
-    TreeNode root = new TreeNode(preorder[preStart]);
-    int inIndex = 0; // Index of current root in inorder
-    for (int i = inStart; i <= inEnd; i++) {
-        if (inorder[i] == root.val) {
-            inIndex = i;
+public class Solution {
+    private int findPosition(int[] arr, int start, int end, int key) {
+        int i;
+        for (i = start; i <= end; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
         }
+        return -1;
     }
-    root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-    root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
-    return root;
+
+    private TreeNode myBuildTree(int[] inorder, int instart, int inend,
+            int[] preorder, int prestart, int preend) {
+        if (instart > inend) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[prestart]);
+        int position = findPosition(inorder, instart, inend, preorder[prestart]);
+
+        root.left = myBuildTree(inorder, instart, position - 1,
+                preorder, prestart + 1, prestart + position - instart);
+        root.right = myBuildTree(inorder, position + 1, inend,
+                preorder, position - inend + preend + 1, preend);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (inorder.length != preorder.length) {
+            return null;
+        }
+        return myBuildTree(inorder, 0, inorder.length - 1, preorder, 0, preorder.length - 1);
+    }
 }
