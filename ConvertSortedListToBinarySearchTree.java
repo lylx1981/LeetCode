dadfd886068fcfe07ce708f0b2d156a1b0314bbe
lylx1981 <drxuli@gmail.com>
2017-01-20 23:00:28 -0500

    Update ConvertSortedListToBinarySearchTree.java

--- ConvertSortedListToBinarySearchTree.java
+++ ConvertSortedListToBinarySearchTree.java
@@ -26,4 +26,27 @@ public class Solution {
         return thead;
 
     }
+    
+    //上面的是top-down的（每次都要用快慢指针找中点，很慢），还有一种down-top的，复杂度更低，只有O（n），但是比较难理解。
+    //可以参看 http://bangbingsyb.blogspot.com/2014/11/leetcode-convert-sorted-list-to-binary.html
+     /**
+     * Bottom up approach, O(n) (Instead of top-down, O(nlogn)) 
+     * 
+     * STEP 1: Take left n/2 nodes and recursively construct the left sub tree.
+     * 
+     * STEP 2: After left sub tree is constructed, we allocate memory for root
+     * and link the left sub tree with root.
+     * 
+     * STEP 3: Finally, we recursively construct the right sub tree and link it
+     * with root.
+     */
+    public static TreeNode buildTree(int n) {
+        if (n == 0) return null;
+        TreeNode node = new TreeNode(0);
+        node.left = buildTree(n / 2);
+        node.val = cur.val;
+        cur = cur.next;
+        node.right = buildTree(n - n / 2 - 1); // why n - n/2 - 1? The # of nodes in right subtreeis total nodes - nodes in left subtree - root
+        return node;
+    }
 }
