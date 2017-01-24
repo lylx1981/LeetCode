8cd47a4a06384f6e5b06ebeef737c9732bd93473
lylx1981 <drxuli@gmail.com>
2017-01-24 10:08:00 -0500

    Create InvertBinaryTree.java

---
+++ InvertBinaryTree.java
@@ -0,0 +1,18 @@
+//经典递归
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertBinaryTree(root.left);
+        invertBinaryTree(root.right);
+    }
+}
