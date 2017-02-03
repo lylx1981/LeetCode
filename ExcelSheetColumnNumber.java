db7749a581a76945b85b0822abd65f22893d42f0
lylx1981 <drxuli@gmail.com>
2017-02-03 11:21:47 -0500

    Update ExcelSheetColumnNumber.java

--- ExcelSheetColumnNumber.java
+++ ExcelSheetColumnNumber.java
@@ -1,5 +1,5 @@
  
-    /** 和另外一个Excel Sheet题是姐妹题，这道题好像更简单一点。就是进制转换。
+    /** 和另外一个Excel Sheet题是姐妹题，这道题好像更简单一点。就是进制转换。同时这一次直接从前往后走，也就是从高位往地位走。
      * Go through the title
      * Map A ~ Z to 1 ~ 26
      * next result = current res * 26 + number of current letter
