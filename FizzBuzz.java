529fe02ef81e88e25d95ba6daadc92681745a1e5
lylx1981 <drxuli@gmail.com>
2017-02-06 10:31:32 -0500

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,25 @@
+/**
+ * 思路：没什么技巧，非常简单的一道题，每次整除3，5以及15，然后根据是否能整除，决定结果即可。
+ */
+
+class Solution {
+    /**
+     * param n: As description.
+     * return: A list of strings.
+     */
+    public ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                results.add("fizz buzz");
+            } else if (i % 5 == 0) {
+                results.add("buzz");
+            } else if (i % 3 == 0) {
+                results.add("fizz");
+            } else {
+                results.add(String.valueOf(i));
+            }
+        }
+        return results;
+    }
+}
