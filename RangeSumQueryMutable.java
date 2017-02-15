5f470a90846a6687e86ad9d702464fc97cac529e
lylx1981 <drxuli@gmail.com>
2017-02-15 15:29:53 -0500

    Create RangeSumQueryMutable.java

---
+++ RangeSumQueryMutable.java
@@ -0,0 +1,104 @@
+    /*
+思路：用BIT 树状数组，看了很久还是不怎么能看懂。
+
+本来以为是比较简单的题，但是发现简单的题过不了Leetcode，主要这道题的难点在于Query和Update一样多，需要兼顾两者，注意看下面的描述：
+
+一个简单的方案是从l到r执行循环，计算给定区间的元素之和。更新值的时候，简单地令arr[i] = x。第一个操作花费O(n)的时间，第二个操作花费O(1)的时间。
+
+第二个方案是创建另外一个数组来存储从下标i开始的元素之和。这样一来，给定区间之和可以用O(1)的时间计算，但是更新需要花费O(n)的时间。这种方法适用于需要大量查询而更新操作较少的场景。
+
+如果查询和更新的次数一样多呢？我们可以在O(log n)的时间内完成上述两种操作吗？
+
+
+
+可以看看如下帖子：
+
+https://www.hackerearth.com/practice/notes/binary-indexed-tree-made-easy-2/
+
+ 
+
+    */        
+        
+public class NumArray {
+	/**
+	 * Binary Indexed Trees (BIT or Fenwick tree):
+	 * https://www.topcoder.com/community/data-science/data-science-
+	 * tutorials/binary-indexed-trees/
+	 * 
+	 * Example: given an array a[0]...a[7], we use a array BIT[9] to
+	 * represent a tree, where index [2] is the parent of [1] and [3], [6]
+	 * is the parent of [5] and [7], [4] is the parent of [2] and [6], and
+	 * [8] is the parent of [4]. I.e.,
+	 * 
+	 * BIT[] as a binary tree:
+	 *            ______________*
+	 *            ______*
+	 *            __*     __*
+	 *            *   *   *   *
+	 * indices: 0 1 2 3 4 5 6 7 8
+	 * 
+	 * BIT[i] = ([i] is a left child) ? the partial sum from its left most
+	 * descendant to itself : the partial sum from its parent (exclusive) to
+	 * itself. (check the range of "__").
+	 * 
+	 * Eg. BIT[1]=a[0], BIT[2]=a[1]+BIT[1]=a[1]+a[0], BIT[3]=a[2],
+	 * BIT[4]=a[3]+BIT[3]+BIT[2]=a[3]+a[2]+a[1]+a[0],
+	 * BIT[6]=a[5]+BIT[5]=a[5]+a[4],
+	 * BIT[8]=a[7]+BIT[7]+BIT[6]+BIT[4]=a[7]+a[6]+...+a[0], ...
+	 * 
+	 * Thus, to update a[1]=BIT[2], we shall update BIT[2], BIT[4], BIT[8],
+	 * i.e., for current [i], the next update [j] is j=i+(i&-i) //double the
+	 * last 1-bit from [i].
+	 * 
+	 * Similarly, to get the partial sum up to a[6]=BIT[7], we shall get the
+	 * sum of BIT[7], BIT[6], BIT[4], i.e., for current [i], the next
+	 * summand [j] is j=i-(i&-i) // delete the last 1-bit from [i].
+	 * 
+	 * To obtain the original value of a[7] (corresponding to index [8] of
+	 * BIT), we have to subtract BIT[7], BIT[6], BIT[4] from BIT[8], i.e.,
+	 * starting from [idx-1], for current [i], the next subtrahend [j] is
+	 * j=i-(i&-i), up to j==idx-(idx&-idx) exclusive. (However, a quicker
+	 * way but using extra space is to store the original array.)
+	 */
+
+	int[] nums;
+	int[] BIT;
+	int n;
+
+	public NumArray(int[] nums) {
+		this.nums = nums;
+
+		n = nums.length;
+		BIT = new int[n + 1];
+		for (int i = 0; i < n; i++)
+			init(i, nums[i]);
+	}
+
+	public void init(int i, int val) {
+		i++;
+		while (i <= n) {
+			BIT[i] += val;
+			i += (i & -i);
+		}
+	}
+
+	void update(int i, int val) {
+		int diff = val - nums[i];
+		nums[i] = val;
+		init(i, diff);
+	}
+
+	public int getSum(int i) {
+		int sum = 0;
+		i++;
+		while (i > 0) {
+			sum += BIT[i];
+			i -= (i & -i);
+		}
+		return sum;
+	}
+
+	public int sumRange(int i, int j) {
+		return getSum(j) - getSum(i - 1);
+	}
+}
