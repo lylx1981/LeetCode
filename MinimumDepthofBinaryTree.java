ed143cfa1638c553cf8d7e370d4f1a342954e5e9
lylx1981 <drxuli@gmail.com>
2018-04-28 15:22:46 -0400

    Update MinimumDepthofBinaryTree.java

--- MinimumDepthofBinaryTree.java
+++ MinimumDepthofBinaryTree.java
@@ -2,6 +2,22 @@
  * 经典的递归即可
  */
 
+
+Xu 4/28/2018
+
+Leetcode上推荐的更好的代码：
+
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
+       
+    }
+}
+
+
 public class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) {
